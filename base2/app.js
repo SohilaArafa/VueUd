ca909fd273d1b47086fadb6ae31122a16023014a
SohilaArafa <sohilaarafa2001@gmail.com>
2023-02-08 16:53:19 +0200

    section2-3

--- base2/app.js
+++ base2/app.js
@@ -2,8 +2,25 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name : ''
     };
   },
+
+  methods:{ 
+    increment(n) {
+     this.counter = this.counter + n;
+  },
+    decrement(n) {
+      this.counter = this.counter - n;
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    submotForm() {
+      alert('Submitted')
+    }
+}
+
 });
 
 app.mount('#events');
