f7a8a14e52300b1d597911ecbd95cf0b9c2ed4e0
SohilaArafa <sohilaarafa2001@gmail.com>
2023-02-09 14:06:36 +0200

    special attack

--- monsterGame/app.js
+++ monsterGame/app.js
@@ -6,18 +6,37 @@ const app = Vue.createApp({
     data(){
         return {
             playerHealth : 100,
-            monsterHealth : 100
+            monsterHealth : 100,
+            currentRound : 0
+        }
+    },
+    computed : {
+        monsterBarStyle() {
+            return {width: this.monsterHealth + '%' };
+        },
+        playerBarStyle() {
+            return {width: this.playerHealth + '%' };
+        },
+        activate() {
+            return this.currentRound % 3 !== 0;
         }
     },
     methods : {
         attackMonster() {
             const attackValue = getRandom(5,12);
             this.monsterHealth -= attackValue; 
+            this.currentRound++;
             this.attackPlayer();
         },
         attackPlayer() {
             const attackValue = getRandom(8,15);
             this.playerHealth -= attackValue; 
+        },
+        specialAttackMons() {
+            const attackValue = getRandom(10,25);
+            this.monsterHealth -= attackValue; 
+            this.currentRound++;
+            this.attackPlayer();
         }
     }
 });
