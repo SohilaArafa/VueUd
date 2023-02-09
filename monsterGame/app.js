5b977d973c03a88409434e93b866eee61c347d50
SohilaArafa <sohilaarafa2001@gmail.com>
2023-02-09 15:00:30 +0200

    surrender button and control sec

--- monsterGame/app.js
+++ monsterGame/app.js
@@ -78,6 +78,9 @@ const app = Vue.createApp({
             this.monsterHealth = 100;
             this.currentRound = 0;
             this.winner = null;            
+        },
+        surrender() {
+            this.winner = "monster";
         }
     }
 });
