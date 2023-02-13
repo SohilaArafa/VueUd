143749d4458ad96813ce3bf8c651d4206f0eb672
SohilaArafa <sohilaarafa2001@gmail.com>
2023-02-13 15:40:43 +0200

    adding a component

---
+++ section7/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<section>
+<h2>My Friends</h2>
+<ul>
+    <li>
+
+    </li>
+    </ul> 
+</section>     
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends : [ {
+                id : 'wezza' ,
+                name : 'Ahmed Nasser',
+                phone : '01117004719' ,
+                email : 'wezza@mail.com'
+            }, {
+                id : 'emz' ,
+                name : 'Eman Arafa',
+                phone : '01117004718' ,
+                email : 'emz@mail.com'
+            }]
+
+        }
+        
+    }
+}
+</script>
