95218d6015adb1dcb6162b135aa14dfe820edefb
SohilaArafa <sohilaarafa2001@gmail.com>
2023-02-14 16:50:14 +0200

    prettier props

--- section8/src/components/FriendContact.vue
+++ section8/src/components/FriendContact.vue
@@ -20,12 +20,28 @@
 
 <script>
 export default {
-  props : [
-    'name',
-    'phoneNumber' ,
-    'emailAddress',
-    'isFavourite'
-  ] ,
+  props : {
+    name : {
+      type : String ,
+      required : true
+    } ,
+    phoneNumber : {
+      type : String ,
+      required : true
+    } ,
+    emailAddress : {
+      type : String ,
+      required : true
+    },
+    isFavourite : {
+      type : String ,
+      required : false ,
+      default : '0',
+      validator : (value) => {
+        return value === '1' || value === '0';
+      }
+    }
+  } ,
   data() {
     return {
       detailsAreVisible: false,
