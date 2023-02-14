70f95892bbd6a29edfe925aed338dfba08fd6304
SohilaArafa <sohilaarafa2001@gmail.com>
2023-02-14 16:00:50 +0200

    sec7

---
+++ section7/src/components/FriendsContact.vue
@@ -0,0 +1,21 @@
+<template>
+    <li>
+        <h2> {{friends.name}} </h2>
+        <button>Show Details</button>
+        <ul>
+            <li><strong>Phone:  </strong> {{friends.phone}} </li>
+            <li><strong>email:  </strong> {{friends.email}} </li>
+
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
