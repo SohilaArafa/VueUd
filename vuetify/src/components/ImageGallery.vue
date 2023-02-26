f39dff891d01e24ee7cd7e68e5a14ab6073fdc7a
SohilaArafa <sohilaarafa2001@gmail.com>
2023-02-26 17:23:49 +0200

    images

--- vuetify/src/components/ImageGallery.vue
+++ vuetify/src/components/ImageGallery.vue
@@ -8,6 +8,8 @@
             <v-card>
                 <v-img
                     :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                    aspect-ratio="1"
+                    cover
                   >
                 </v-img>
             </v-card>
