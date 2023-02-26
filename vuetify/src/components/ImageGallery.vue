bae3eb38c29fbed41cad75482deb2be1c25de5f9
SohilaArafa <sohilaarafa2001@gmail.com>
2023-02-26 17:31:48 +0200

    responsive window

--- vuetify/src/components/ImageGallery.vue
+++ vuetify/src/components/ImageGallery.vue
@@ -3,11 +3,15 @@
         <v-col 
          v-for="n in 200"
          :key="n"
-         cols="1"
+         cols="4"
+         sm="3"
+         md="2"
+         lg="1"
         >
             <v-card>
                 <v-img
                     :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                    :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
                     aspect-ratio="1"
                     cover
                 >
