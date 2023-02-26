156231781561efa32d633417ebb5537eafb3d640
SohilaArafa <sohilaarafa2001@gmail.com>
2023-02-26 16:39:49 +0200

    side bar check

--- vuetify/src/App.vue
+++ vuetify/src/App.vue
@@ -1,7 +1,17 @@
+<script setup>
+import SideBar from './components/SideBar.vue';
+</script>
+
 <template>
-<div>Hi</div>
+<div>
+<v-app>
+  <!--SIDEBAR-->
+  <SideBar/>
+  <v-main>
+    <!--cards-->
+  </v-main>
+</v-app>
+</div>
 </template>
 
-<script>
 
-</script>
---
+++ vuetify/src/components/SideBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-navigation-drawer
+    expand-on-hover
+    rail
+    >
+    <v-list>
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+        title="Sohila Arafa"
+        subtitle="sohila@gmail.com"
+      ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-image" title="Color Gallery" value="color"></v-list-item>
+        <v-list-item prepend-icon="mdi-dialpad" title="GraySpace Gallery" value="shared"></v-list-item>
+        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+     </v-list>
+
+    </v-navigation-drawer>
+</template>
