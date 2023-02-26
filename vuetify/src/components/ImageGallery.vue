6e164687bdfbee6168d2ac97a64ab1a1b199c9e5
SohilaArafa <sohilaarafa2001@gmail.com>
2023-02-26 17:27:58 +0200

    loading indicator

--- vuetify/src/components/ImageGallery.vue
+++ vuetify/src/components/ImageGallery.vue
@@ -10,7 +10,16 @@
                     :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
                     aspect-ratio="1"
                     cover
-                  >
+                >
+                <template v-slot:placeholder>
+                    <v-row class="fill-height ma-0" align="center" justify="center" >
+                        <v-progress-circular
+                           indeterminate
+                           color="grey-lighten-5"
+                        >
+                        </v-progress-circular>
+                    </v-row>
+                </template>
                 </v-img>
             </v-card>
         </v-col>
